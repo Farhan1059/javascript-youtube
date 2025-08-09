0872f49f4059389112118989c3057cad34a4e67a
farhan <farhanjaffer24@gmail.com>
2025-08-09 08:46:44 -0700

    Conversion is to modify

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -24,6 +24,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 555          // no write it in SomeNumber/Somenumber/somenumber only in someNumber
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ************************ Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 raise to power 2
+// console.log(2/2);
+// console.log(2%2); // modulus/remainder
+
+let str1 = "hey"
+let str2 = " farhan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
